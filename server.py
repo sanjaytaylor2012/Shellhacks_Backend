0c7585c2040ccac3b590dd341f72a59e6d1637a9
AnthonyYao7 <anthonyjyao@gmail.com>
2023-09-16 12:06:24 -0400

    added cors ability

--- server.py
+++ server.py
@@ -1,36 +1,35 @@
 import os
 import urllib.request
 from flask import Flask, flash, request, redirect, url_for, render_template
+from flask_cors import CORS
 
 
 UPLOAD_FOLDER = "static/uploads/"
 
 app = Flask(__name__)
+CORS(app)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 
-@app.route('/')
-def upload_form():
-    return render_template()
-
-
-@app.route('/', methods=['POST'])
+@app.route('/upload', methods=['POST'])
 def upload_video():
     if 'file' not in request.files:
         flash('No file part')
+        print("No file part")
         return redirect(request.url)
     file = request.files['file']
     if file.filename == '':
         flash('No image selected for uploading')
+        print("No image selected for uploading")
         return redirect(request.url)
     else:
         filename = file.filename
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        #print('upload_video filename: ' + filename)
         flash('Video successfully uploaded and displayed below')
-        return render_template('upload.html', filename=filename)
+        print("Video successfully uploaded and displayed below")
+        return redirect(request.url)
 
 
 @app.route('/display/<filename>')
